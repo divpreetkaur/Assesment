5170f9455473d016718e038ee8179f391844076d
divpreet <divpreetkaur21@gmail.com>
2019-05-14 20:36:53 +0530

    recyclerView

--- app/src/main/java/com/example/recyclerview/my_text_view.java
+++ app/src/main/java/com/example/recyclerview/my_text_view.java
@@ -12,10 +12,5 @@ public class my_text_view extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_text_view);
     }
-    void count(View view)
-    {
-        TextView textView =(TextView)findViewById(R.id.text_tv_count);
-        count_likes++;
-        textView.setText(" "+count_likes);
-    }
+
 }
